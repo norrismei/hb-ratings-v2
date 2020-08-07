5c30c282da644a39c4cee9fb501830c296391a0e
Norris Mei <norrismei3@gmail.com>
2020-08-07 01:04:45 +0000

    Completed all movies, movie details, all users, and user profile pages

--- crud.py
+++ crud.py
@@ -13,6 +13,15 @@ def create_user(email, password):
 
     return user
 
+def get_users():
+    """get a list of all users"""
+
+    return db.session.query(User).all()
+
+def get_user_by_id(user_id):
+    """Return the user object by ID"""
+
+    return db.session.query(User).filter_by(user_id=user_id).one()
 
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie."""
@@ -25,6 +34,18 @@ def create_movie(title, overview, release_date, poster_path):
     return movie
 
 
+def get_movies():
+    """Returns a list of all movies in database"""
+
+    return db.session.query(Movie).all()
+
+
+def get_movie_by_id(movie_id):
+    """Return movie with that id"""
+
+    return db.session.query(Movie).filter_by(movie_id=movie_id).one()
+
+
 def create_rating(score, user, movie):
     """Create a rating"""
 
@@ -34,6 +55,8 @@ def create_rating(score, user, movie):
     db.session.commit()
 
     return rating
+
+
 
 if __name__ == '__main__':
     from server import app
--- server.py
+++ server.py
@@ -1,12 +1,58 @@
 """Server for movie ratings app."""
+from flask import (Flask, render_template, request, flash, session,
+                   redirect)
+from model import connect_to_db
+import crud
 
-from flask import Flask
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 
 
-# Replace this with routes and view functions!
+@app.route('/')
+def show_homepage():
+    """View homepage"""
+
+    return render_template('homepage.html')
+
+
+@app.route('/movies')
+def all_movies():
+    """View all movies"""
+
+    movies = crud.get_movies()
+
+    return render_template('all_movies.html', movies=movies)
+
+
+@app.route('/movies/<movie_id>')
+def movie_details(movie_id):
+    """View details about one movie"""
+
+    movie = crud.get_movie_by_id(movie_id)
+
+    return render_template('movie_details.html', movie=movie)
+
+@app.route('/users')
+def get_list_of_users():
+    """View List of Users"""
+
+    users = crud.get_users()
+
+    return render_template('users.html', users=users)
+
+@app.route('/users/<user_id>')
+def get_user_profile(user_id):
+    """View user profile details"""
+    user = crud.get_user_by_id(user_id)
+
+    return render_template('user_profile.html', user=user)
+
+
 
 
 if __name__ == '__main__':
+    connect_to_db(app)
     app.run(host='0.0.0.0', debug=True)
